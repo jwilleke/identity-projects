a101194a52cc255b384d972aa59c72d6a67a34b3
activescott <activescott@49064eca-e600-e74d-4954-46bbb9c2e515>
2010-11-17 02:49:08 +0000

    putting 1.0 in a branch for code review

---
+++ branches/1.0/dotnet/NovellX.Ldap/Security.Nmas/NativeMethods.NmasLdap.cs
@@ -0,0 +1,57 @@
+﻿// Authors:
+// Scott Willeke (scott@willeke.com)
+//
+// Copyright (c) 2010 Scott Willeke (scott@willeke.com)
+// Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
+using System;
+using System.Runtime.InteropServices;
+
+namespace NovellX.Security.Nmas
+{
+	internal static partial class NativeMethods
+	{
+		/// <summary>
+		/// Contains native methods from the NMAS native libraries.
+		/// </summary>
+		internal static class NmasLdap
+		{
+			public static readonly int E_BASE = (-1600);
+			public static readonly int E_SERVER_NOT_FOUND = (E_BASE - 64)     /* -1664 0xFFFFF980 */;
+
+			/// <summary>
+			/// Reads the password.
+			/// </summary>
+			/// <param name="ld">(IN) The LDAP session handle. </param>
+			/// <param name="objectDN">
+			/// (IN) Identifies the object that holds the login data. An LDAP DN. 
+			/// C: UTF-8 string
+			/// </param>
+			/// <param name="pwdLen">
+			/// (OUT) Password length in bytes.
+			/// C: size_t int</param>
+			/// <param name="pwd">
+			/// (OUT) Specifies the password.
+			/// C: UTF-8 string
+			/// </param>
+			/// <returns>
+			/// Returns NMAS_E_SUCCESS (0) if successful or a non-zero NMAS Error Code if not successful.
+			/// </returns>
+			/// <remarks>
+			/// <code>
+			/// #include &lt;ldap.h&gt;
+			/// #include &lt;ntypes.h&gt;
+			/// #include &lt;nmasext.h&gt;
+			/// int nmasldap_get_password
+			/// (
+			///      LDAP     *ld,
+			///      char     *objectDN,
+			///      size_t   *pwdLen,   
+			///      char     *pwd 
+			/// );
+			/// </code>
+			/// </remarks>
+			[DllImport("nmasExt.dll", EntryPoint = "nmasldap_get_password", CallingConvention = CallingConvention.Cdecl)]
+			public static extern int get_password(IntPtr ld, [MarshalAs(UnmanagedType.LPStr)] string objectDN, IntPtr pwdLen, [MarshalAs(UnmanagedType.LPStr)] System.Text.StringBuilder pwd);
+		}
+	}
+}
---
+++ branches/1.0/dotnet/NovellX.Ldap/Security.Nmas/NmasLdap.cs
@@ -0,0 +1,43 @@
+﻿// Authors:
+// Scott Willeke (scott@willeke.com)
+//
+// Copyright (c) 2010 Scott Willeke (scott@willeke.com)
+// Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
+using System;
+using System.Diagnostics;
+using System.Text;
+using NovellX.Ldap;
+
+namespace NovellX.Security.Nmas
+{
+	public class NmasLdap
+	{
+		private readonly LdapConnection _ldapConnection;
+
+		public NmasLdap(LdapConnection ldapConnection)
+		{
+			if (ldapConnection == null)
+				throw new ArgumentNullException("ldapConnection");
+			_ldapConnection = ldapConnection;
+		}
+
+		public string GetPassword(string getPasswordUserDN)
+		{
+			const int getPwdSize = 512;
+			var getPwd = new StringBuilder(512);
+			var getPwdLen = getPwdSize;
+			int err;
+			unsafe {
+				err = NativeMethods.NmasLdap.get_password(_ldapConnection.Handle, getPasswordUserDN, new IntPtr(&getPwdLen), getPwd);
+			}
+
+			if (err == NovellX.Ldap.NativeMethods.Ldap.SUCCESS) {
+				//Debug.WriteLine("nmasldap_get_password: {0}", getPwd);
+			}
+			else {
+				Debug.WriteLine("ERROR {0} nmasldap_get_password", err);
+			}
+			return getPwd.ToString(0, getPwdLen);
+		}
+	}
+}
